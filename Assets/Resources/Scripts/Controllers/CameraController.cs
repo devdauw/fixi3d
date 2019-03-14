b856ba25be99c67089c4a174dd6051848338e6b2
Massimo Gentile <massimo.gentile@condorcet.be>
2019-03-14 16:51:35 +0100

    Moving Camera Complete
    We are now able to move to camera, only the position for the 2D camera and for the 3D camera we are able to move the position and also the rotation.
    The zoom function is also implemented in the 3D by scrolling with mouse.

--- Assets/Resources/Scripts/Controllers/CameraController.cs
+++ Assets/Resources/Scripts/Controllers/CameraController.cs
@@ -3,6 +3,7 @@
 public class CameraController : MonoBehaviour
 {
     public Camera[] Cameras;
+    private float moveSpeed = 0.125f;
     private int _currentCameraIndex;// 0 : 2D camera
                                     // 1 : 3D Ortho Camera
 
@@ -43,15 +44,104 @@ public class CameraController : MonoBehaviour
             // Mouse Wheel Down
             case 1:
                 var sizeD = Cameras[_currentCameraIndex].orthographicSize;
-                sizeD += (float)0.2;
+                sizeD += 0.2f;
                 Cameras[_currentCameraIndex].orthographicSize = sizeD;
                 break;
             // Mouse Wheel Up
             case -1:
                 var sizeU = Cameras[_currentCameraIndex].orthographicSize;
-                sizeU -= (float)0.2;
+                sizeU -= 0.2f;
                 Cameras[_currentCameraIndex].orthographicSize = sizeU;
                 break;
+            default:
+                return;
         }
+    }
+
+    void MoveCamera(string direction)
+    {
+        var pos = Cameras[_currentCameraIndex].transform.position;
+        var rot = Cameras[_currentCameraIndex].transform.eulerAngles;
+        switch (_currentCameraIndex)
+        {
+            // 2D Camera
+            case 0:
+                switch (direction)
+                {
+                    case "Left":
+                        pos.x -= 0.1f;
+                        break;
+                    case "Top":
+                        pos.y += 0.1f;
+                        break;
+                    case "Right":
+                        pos.x += 0.1f;
+                        break;
+                    case "Bottom":
+                        pos.y -= 0.1f;
+                        break;
+                    default:
+                        return;
+                }
+                break;
+
+            // 3D Ortho Camera
+            case 1:
+                switch (direction)
+                {
+                    case "Left":
+                        pos.x -= moveSpeed;
+                        pos.y += moveSpeed / 10;
+                        pos.z -= moveSpeed * 1.9f;
+                        break;
+                    case "Top":
+                        pos.x -= moveSpeed / 4;
+                        pos.y += moveSpeed;
+                        pos.z += moveSpeed / 10;
+                        break;
+                    case "Right":
+                        pos.x += moveSpeed;
+                        pos.y -= moveSpeed / 10;
+                        pos.z += moveSpeed * 1.9f;
+                        break;
+                    case "Bottom":
+                        pos.x += moveSpeed / 4;
+                        pos.y -= moveSpeed;
+                        pos.z -= moveSpeed / 10;
+                        break;
+                    case "CtrlLeft":
+                        rot.y -= 0.1f;
+                        Cameras[1].transform.eulerAngles = rot;
+                        break;
+                    case "CtrlTop":
+                        rot.x -= 0.1f;
+                        Cameras[1].transform.eulerAngles = rot;
+                        break;
+                    case "CtrlRight":
+                        rot.y += 0.1f;
+                        Cameras[1].transform.eulerAngles = rot;
+                        break;
+                    case "CtrlBottom":
+                        rot.x += 0.1f;
+                        Cameras[1].transform.eulerAngles = rot;
+                        break;
+                    default:
+                        return;
+                }
+                break;
+
+            default:
+                return;
+        }
+        Cameras[_currentCameraIndex].transform.position = pos;
+    }
+
+    private void Move(Vector3 m)
+    {
+        var pos = Cameras[_currentCameraIndex].transform.position;
+        pos.x -= m.x;
+        pos.y += m.y;
+        pos.z += m.z;
+        Cameras[_currentCameraIndex].transform.position = pos;
     }
 }
