42b1986c5cfbc5dc5ed068e732ac21648fef4cf3
Massimo Gentile <massimo.gentile@condorcet.be>
2019-03-22 09:22:21 +0100

    .Fbx Fixation Test

--- Assets/Resources/Scripts/Controllers/FixationCreator.cs
+++ Assets/Resources/Scripts/Controllers/FixationCreator.cs
@@ -6,11 +6,10 @@ public class FixationCreator : MonoBehaviour
 {
     public void CreateFix()
     {
-        /*Object prefab = Resources.Load("Fixations/Suspente27kN-3DView-{3D}");
+        Object prefab = UnityEngine.Resources.Load("Fixations/YFix");
         GameObject t = (GameObject)Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
         Camera cam = t.AddComponent<Camera>();
         cam.transform.position = new Vector3(11.16f, 6.59f, -2.62f);
         cam.transform.Rotate(new Vector3(18.141f, -33.983f, 1.991f));
-        */
     }
 }
