d8a17f1fecf7110cf6fd342961c48cdec59f739f
Massimo Gentile <massimo.gentile@condorcet.be>
2019-04-03 10:12:42 +0200

    Fix Edit/Remove/CopyPaste
    I have made some changement on the way to select the wall so I refactor all the function.

--- Assets/Resources/Scripts/Controllers/WallCreator.cs
+++ Assets/Resources/Scripts/Controllers/WallCreator.cs
@@ -23,7 +23,6 @@ public class WallCreator : MonoBehaviour
     #endregion
 
     public List<Model3D> modelSList = new List<Model3D>();
-    public GameObject Suspente;
     private static int _wallNum = 0;
 
     private void Start() {
@@ -36,45 +35,37 @@ public class WallCreator : MonoBehaviour
     void Update()
     {
         //Check pour verifier que l'on est sur le plan en 2D afin d'eviter les null exception
-        if (Camera.main != null)
+        if (Camera.main == null) return;
+        if (Input.GetMouseButtonDown(0))
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                _draggingMouse = true;
-                _mousePositions[0] = Input.mousePosition;
-            }
+            _draggingMouse = true;
+            _mousePositions[0] = Input.mousePosition;
+        }
 
-            if (Input.GetMouseButtonUp(0))
-            {
-                _draggingMouse = false;
+        if (!Input.GetMouseButtonUp(0)) return;
+        _draggingMouse = false;
             
-                //On transform les donnees de notre souris pour les faire correspondre aux coordonnees de notre monde
-                var beginPos = Camera.main.ScreenToWorldPoint(_mousePositions[0]);
-                _mousePositions[1] = Input.mousePosition;
-                var endPos = Camera.main.ScreenToWorldPoint(_mousePositions[1]);
+        //On transform les donnees de notre souris pour les faire correspondre aux coordonnees de notre monde
+        var beginPos = Camera.main.ScreenToWorldPoint(_mousePositions[0]);
+        _mousePositions[1] = Input.mousePosition;
+        var endPos = Camera.main.ScreenToWorldPoint(_mousePositions[1]);
 
-                if (Math.Abs(beginPos.x - endPos.x) > 1)
-                {
-                    var topCorner = Math.Min(beginPos.x, endPos.x);
-                    var bottomCorner = Math.Min(beginPos.y, endPos.y);
-                    var width = Math.Max(beginPos.x, endPos.x) - topCorner;
-                    var height = Math.Max(beginPos.y, endPos.y) - bottomCorner;
+        if (!(Math.Abs(beginPos.x - endPos.x) > 1)) return;
+        var topCorner = Math.Min(beginPos.x, endPos.x);
+        var bottomCorner = Math.Min(beginPos.y, endPos.y);
+        var width = Math.Max(beginPos.x, endPos.x) - topCorner;
+        var height = Math.Max(beginPos.y, endPos.y) - bottomCorner;
             
-                    CreateWall(width, height, topCorner, bottomCorner); 
-                }      
-            } 
-        } 
+        CreateWall(width, height, topCorner, bottomCorner);
     }
 
     //Nous permet de tracer un rectangle de selection par dessus notre canvas pour voir ce que l'on trace
     void OnGUI()
     {
-        if (_draggingMouse)
-        {
-            var rect = Utils.GetMousePositions(_mousePositions[0], Input.mousePosition);
-            Utils.DrawMouseRect(rect, new Color( 0.8f, 0.8f, 0.95f, 0.25f ));
-            Utils.DrawMouseRectBorder( rect, 2, new Color( 0.8f, 0.8f, 0.95f ) ); 
-        }
+        if (!_draggingMouse) return;
+        var rect = Utils.GetMousePositions(_mousePositions[0], Input.mousePosition);
+        Utils.DrawMouseRect(rect, new Color( 0.8f, 0.8f, 0.95f, 0.25f ));
+        Utils.DrawMouseRectBorder( rect, 2, new Color( 0.8f, 0.8f, 0.95f ) );
     }
    
     //Create a wall using our page input fields
@@ -108,53 +99,51 @@ public class WallCreator : MonoBehaviour
     public void CopyWall(string selectedWall)
     {
         var model = JsonUtility.FromJson<SzModel>(selectedWall);
-        foreach (var item in modelSList.Where(x => x.Name == model.modelName))
-        {
-            var copyWall = new Model3D();
-            copyWall.CreateModel(item.Position.x + item.Size.x, item.Position.y, item.Position.z,
-                item.Size.x, item.Size.y, item.Size.z, "Wall" + _wallNum, "Green");
-            _wallNum++;
-            copyWall.Model.gameObject.tag = "FixiWalls";
-            modelSList.Add(copyWall);
-            #if !UNITY_EDITOR && UNITY_WEBGL
-                SendWallsList();
-            #endif
-        }
+        var selector = GameObject.Find("MouseManager");
+        var walls = modelSList.Where(x => x.Name == model.modelName).ToList();
+        selector.SendMessage("ClearSelection");
+        var copyWall = new Model3D();
+        copyWall.CreateModel(walls.First().Position.x + walls.First().Size.x, walls.First().Position.y, walls.First().Position.z,
+            walls.First().Size.x, walls.First().Size.y, walls.First().Size.z, "Wall" + _wallNum, "Green");
+        _wallNum++;
+        copyWall.Model.gameObject.tag = "FixiWalls";
+        modelSList.Add(copyWall);
+        #if !UNITY_EDITOR && UNITY_WEBGL
+            SendWallsList();
+        #endif
     }
 
     private void EditWall(string selectedWall)
     {
         var model = JsonUtility.FromJson<SzModel>(selectedWall);
-        foreach (var item in modelSList.Where(x => x.Name == model.modelName))
-        {
-            var size = item.Model.GetComponent<Renderer>().bounds.size;
-            var rescale = item.Model.transform.localScale;
-            var newSize = new Vector3(GetFloatValueFromInput("input_edit_length"), GetFloatValueFromInput("input_edit_height"), GetFloatValueFromInput("input_edit_width"));
-            rescale.x = newSize.x * rescale.x / size.x;
-            rescale.y = newSize.y * rescale.y / size.y;
-            rescale.z = newSize.z * rescale.z / size.z;
-            item.Model.transform.localScale = rescale;
-            item.Size = newSize;
-            #if !UNITY_EDITOR && UNITY_WEBGL
-                SendWallsList();
-            #endif
-        }
+        var walls = modelSList.Where(x => x.Name == model.modelName).ToList();
+        var size = walls.First().Model.GetComponent<Renderer>().bounds.size;
+        var rescale = walls.First().Model.transform.localScale;
+        var newSize = new Vector3(GetFloatValueFromInput("input_edit_length"), GetFloatValueFromInput("input_edit_height"), GetFloatValueFromInput("input_edit_width"));
+        rescale.x = newSize.x * rescale.x / size.x;
+        rescale.y = newSize.y * rescale.y / size.y;
+        rescale.z = newSize.z * rescale.z / size.z;
+        walls.First().Model.transform.localScale = rescale;
+        walls.First().Size = newSize;
+        #if !UNITY_EDITOR && UNITY_WEBGL
+            SendWallsList();
+        #endif
     }
  
     //Destroy the Wall selected
     private void RemoveWall(string selectedWall)
     {
         var model = JsonUtility.FromJson<SzModel>(selectedWall);
-        foreach (var item in modelSList.Where(x => x.Name == model.modelName))
-        {
-            var hiddenWall = item.Model;
-            hiddenWall.SetActive(false);
-            hiddenWall.tag = "Untagged";
-            modelSList.Remove(item);
-            #if !UNITY_EDITOR && UNITY_WEBGL
-                    SendWallsList();
-            #endif
-        }
+        var selector = GameObject.Find("MouseManager");
+        var walls = modelSList.Where(x => x.Name == model.modelName).ToList();
+        selector.SendMessage("ClearSelection");
+        var hiddenWall = walls.First().Model;
+        hiddenWall.SetActive(false);
+        hiddenWall.tag = "Untagged";
+        modelSList.Remove(walls.First());
+        #if !UNITY_EDITOR && UNITY_WEBGL
+            SendWallsList();
+        #endif
     }
 
     //Method that takes our C# walls list and send it back to our webpage using pointers to the adress of the list
@@ -170,16 +159,5 @@ public class WallCreator : MonoBehaviour
 
         //We serialize our list of simple objects and pass it back to our html
         SendWallsToPage(JsonHelper.ToJson(szModelList.ToArray()));
-    }
-
-    //TODO créer une méthode qui permet de renvoyer un mur en fonction du string avec son nom
-    public void PlaceFixation(string selectedWall)
-    {
-        var model = JsonUtility.FromJson<SzModel>(selectedWall);
-        foreach (var item in modelSList.Where(x => x.Name == model.modelName))
-        {
-            var fixationCreator = GameObject.Find("FixationCreator");
-            fixationCreator.SendMessage("CreateFix",item);
-        }
     }
 }
