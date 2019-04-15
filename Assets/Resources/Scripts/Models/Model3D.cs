7450d4ff8a4a07af8ca4bab36517a32dc4f8ce43
Massimo Gentile <massimo.gentile@condorcet.be>
2019-04-15 09:26:54 +0200

    Added Distanceur + position
    Now we have the distanceur and they are placed better.

--- Assets/Resources/Scripts/Models/Model3D.cs
+++ Assets/Resources/Scripts/Models/Model3D.cs
@@ -8,7 +8,8 @@ public class Model3D
     private Vector3 _size;
     private string _name;
     private Material _material;
-    private GameObject _suspente = (GameObject)UnityEngine.Resources.Load("Fixations/Suspente");
+    private readonly GameObject _suspente = (GameObject)UnityEngine.Resources.Load("Fixations/Suspente");
+    private readonly GameObject _distM20 = (GameObject)UnityEngine.Resources.Load("Fixations/DistanceurM20");
 
     public Vector3 Position { get; set; }
     public Vector3 Size { get; set; }
@@ -42,17 +43,15 @@ public class Model3D
         BackLeftTop = new Vector3(Size.x, Size.y, Size.z);
         BackRightTop = new Vector3(0, Size.y, Size.z);
 
-        Vector3[] vertices = GetVertices();
-        int[] triangles = GetTriangles();
-        Vector2[] uvs = new Vector2[vertices.Length];
-        for (int i = 0; i < uvs.Length; i++)
-        {
+        var vertices = GetVertices();
+        var triangles = GetTriangles();
+        var uvs = new Vector2[vertices.Length];
+        for (var i = 0; i < uvs.Length; i++)
             uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
-        }
-
-        Mesh mesh = new Mesh { vertices = vertices, triangles = triangles, uv = uvs };
+        var mesh = new Mesh { vertices = vertices, triangles = triangles, uv = uvs };
 
         mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
 
         Model = new GameObject(Name, typeof(MeshFilter), typeof(MeshRenderer));
         Model.GetComponent<MeshRenderer>().material = Material;
@@ -62,83 +61,76 @@ public class Model3D
         Model.transform.position = Position;
 
         var fixPosX = Model.gameObject.transform.position.x;
-        // +0.35f == la moitier de la taille d'une suspente pour que celle-ci se retrouve bien centrer
-        var fixLeftPosX = fixPosX + ((sizeX * 0.2f) + 0.35f);
-        var fixRightPosX = fixPosX + ((sizeX * 0.8f) + 0.35f);
         var fixPosY = Model.gameObject.transform.position.y;
-        var fixTopPosY = fixPosY + sizeY - 0.25f - 0.4f;
-        var fixCenterPosY = fixPosY + ((sizeY * 0.5f) - 0.4f);
-        var fixBottomPosY = fixPosY + 0.25f;
-        var fixPosZ = Model.gameObject.transform.position.z - 0.23f;
+        var fixPosZ = Model.gameObject.transform.position.z;
 
-        /* Preparation Distanceur
-        //Dist Top Left
-        var distTopLeftX = fixPosX + 0.15f;
-        var distTopLeftY = fixPosY + 0.2f;
+        var suspXLenght = 0.60299994f;
+        var suspYLenght = 0.88800513f;
+        var distXLenght = 0.08148051f;
+        var distYLenght = 0.28499997f;
+
         //Susp Left
-        var suspLeftX = fixPosX + (sizeX * 0.2f);
-        if (suspLeftX - distTopLeftX < 15 || suspLeftX - distTopLeftX > 20)
-            suspLeftX = distTopLeftX + 0.15f;
-        var suspLeftY = fixPosY + sizeY - 0.25f;
-        //Dist Top Right
-        var distTopRightX = fixPosX + sizeX - 0.15f;
-        var distTopRightY = fixPosY + sizeY - 0.2f;
+        var suspLeftX = fixPosX + (sizeX * 0.2f) + suspXLenght;
+        var suspLeftY = fixPosY + sizeY - 0.25f - suspYLenght;
+        InstantiateFixation("Susp", "Left Suspente", suspLeftX, suspLeftY, fixPosZ);
+
+        //Dist Top Left
+        var distTopLeftX = suspLeftX - 0.15f - suspXLenght;
+        var distTopLeftY = fixPosY + sizeY - 0.2f - (distXLenght / 2);
+        InstantiateFixation("DistM20", "TopLeft Distanceur", distTopLeftX, distTopLeftY, fixPosZ);
+
         //Susp Right
         var suspRightX = fixPosX + (sizeX * 0.8f);
-        if (suspRightX - distTopLeftX < 15 || suspRightX - distTopLeftX > 20)
-            suspRightX = distTopRightX - 0.15f;
-        var suspRightY = fixPosY + sizeY - 0.25f;
+        var suspRightY = fixPosY + sizeY - 0.25f - suspYLenght;
+        InstantiateFixation("Susp", "Right Suspente", suspRightX, suspRightY, fixPosZ);
+
+        //Dist Top Right
+        var distTopRightX = suspRightX + 0.15f;
+        var distTopRightY = fixPosY + sizeY - 0.2f - (distXLenght / 2);
+        InstantiateFixation("DistM20", "TopRight Distanceur", distTopRightX, distTopRightY, fixPosZ);
+
         //Dist Bottom Left
         var distBottomLeftX = fixPosX + (sizeX * 0.2f) - 0.2f;
         if (distBottomLeftX - fixPosX < 15)
             distBottomLeftX = fixPosX + 0.15f;
         var distBottomLeftY = fixPosY + 0.15f;
+        InstantiateFixation("DistM20", "BottomLeft Distanceur", distBottomLeftX, distBottomLeftY, fixPosZ);
+
         //Dist Bottom Right
         var distBottomRightX = fixPosX + (sizeX * 0.8f) + 0.2f;
         if ((fixPosX + sizeX) - distBottomRightX < 15)
-            distBottomRightX = fixPosX + sizeX - 0.15f;
-        var distBottomRightY = fixPosY + 0.15f;*/
+            distBottomRightX = fixPosX + sizeX - 0.15f - distXLenght;
+        var distBottomRightY = fixPosY + 0.15f;
+        InstantiateFixation("DistM20", "BottomRight Distanceur", distBottomRightX, distBottomRightY, 0f);
+    }
 
-        //Top Left Fixation
-        var Suspente_TopLeft = (GameObject)Object.Instantiate(_suspente);
-        Suspente_TopLeft.transform.position = new Vector3(fixLeftPosX, fixTopPosY, fixPosZ);
-        Suspente_TopLeft.name = "TopLeft Suspente " + Model.name;
-        Suspente_TopLeft.transform.parent = Model.transform;
-
-        //Top Right Fixation
-        var Suspente_TopRight = (GameObject)Object.Instantiate(_suspente);
-        Suspente_TopRight.transform.position = new Vector3(fixRightPosX, fixTopPosY, fixPosZ);
-        Suspente_TopRight.name = "TopRight Suspente " + Model.name;
-        Suspente_TopRight.transform.parent = Model.transform;
-
-        //Left Fixation
-        var Suspente_Left = (GameObject)Object.Instantiate(_suspente);
-        Suspente_Left.transform.position = new Vector3(fixLeftPosX, fixCenterPosY, fixPosZ);
-        Suspente_Left.name = "Left Suspente " + Model.name;
-        Suspente_Left.transform.parent = Model.transform;
-
-        //Right Fixation
-        var Suspente_Right = (GameObject)Object.Instantiate(_suspente);
-        Suspente_Right.transform.position = new Vector3(fixRightPosX, fixCenterPosY, fixPosZ);
-        Suspente_Right.name = "Right Suspente " + Model.name;
-        Suspente_Right.transform.parent = Model.transform;
-
-        //BottomLeft Fixation
-        var Suspente_BottomLeft = (GameObject)Object.Instantiate(_suspente);
-        Suspente_BottomLeft.transform.position = new Vector3(fixLeftPosX, fixBottomPosY, fixPosZ);
-        Suspente_BottomLeft.name = "BottomLeft Suspente " + Model.name;
-        Suspente_BottomLeft.transform.parent = Model.transform;
-
-        //BottomRight Fixation
-        var Suspente_BottomRight = (GameObject)Object.Instantiate(_suspente);
-        Suspente_BottomRight.transform.position = new Vector3(fixRightPosX, fixBottomPosY, fixPosZ);
-        Suspente_BottomRight.name = "BottomRight Suspente " + Model.name;
-        Suspente_BottomRight.transform.parent = Model.transform;
+    private void InstantiateFixation(string fixType, string name, float x , float y, float z)
+    {
+        GameObject fixation;
+        switch (fixType)
+        {
+            case "DistM20":
+                fixation = (GameObject) Object.Instantiate(_distM20);
+                //fixation.transform.rotation = new Quaternion(-119, -90, 90, fixation.transform.rotation.w);
+                //fixation.transform.rotation = new Quaternion(-118.9f, 87.51199f, -268.583f, fixation.transform.rotation.w);
+                fixation.transform.Rotate(-118.9f, 87.51199f, -268.583f);
+                fixation.transform.position = new Vector3(x + 0.02f, y - 0.03f, z - 0.287f);
+                break;
+            case "Susp":
+                fixation = (GameObject) Object.Instantiate(_suspente);
+                fixation.transform.position = new Vector3(x, y, z - 0.23f);
+                break;
+            default:
+                fixation = new GameObject();
+                break;
+        }
+        fixation.name = name + " " + Model.name;
+        fixation.transform.parent = Model.transform;
     }
 
     private Vector3[] GetVertices()
     {
-        Vector3[] vert = new Vector3[]
+        var vert = new Vector3[]
         {
             //Front Face
             FrontLeftBottom,
@@ -181,7 +173,7 @@ public class Model3D
 
     private int[] GetTriangles()
     {
-        int[] triangles = new int[]
+        var triangles = new int[]
         {
             // Front Face
             0,1,2, //First Triangle
